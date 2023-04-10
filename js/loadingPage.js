8c2ed80baa5e9edd96cb563d22d2014481d0cce5
Rocket0000 <87626472+Rocket0000@users.noreply.github.com>
2023-04-10 12:33:44 +0900

    add func preload

--- js/loadingPage.js
+++ js/loadingPage.js
@@ -8,6 +8,18 @@ class LoadingPage extends HTMLElement{
       <p>로딩중...</p>
     </div>
     `
+let images = [];
+
+function preload() {
+  for(let i = 0; i < preload.arguments.lenght; i++) {
+    images[i] = new Image();
+    images[i].src = preload.arguments.src;
+  }
+}
+
+preload(
+  "../etc/imgs/loading.gif"
+)
   }
 }
 
